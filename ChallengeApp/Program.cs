a643e2b85b4a140381062b0dd0ea0d9674598525
DanielGoIT <kaszudbowski11@wp.pl>
2023-01-28 21:32:53 +0100

    Zadanie domowe dzien 5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,30 +1,69 @@
-﻿/// Napisz w Program.cs kod w którym:
-/// - zadeklarujesz zmienną z imieniem
-/// - zadeklarujesz zmienną z płcią (dobierz zmienną)
-/// - zadeklarujesz zmienną z wiekiem
-/// - zweryfikujesz dane i wyświetlisz
-/// jeden z komunikatów:
-/// (1): "Kobieta poniżej 30 lat"
-/// (2): "Ewa, lat 33"
-/// (3): "Niepełnoletni Mężczyzna"
-/// ---------------------------
-/// (możesz dodać swoje)
-
-string name = "Ewa";
-string sex = "Female";
-int age = 17;
-
-if (name == "Ewa" && sex == "Female" && age == 17)
+﻿int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
 
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-if (sex == "Female" && age > 30)
-
+foreach (char letter in letters)
 {
-    Console.WriteLine("Ewa, lat 33");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
-
-else if (sex == "Female" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+Console.WriteLine($"0=> {counter0}");
+Console.WriteLine($"1=> {counter1}");
+Console.WriteLine($"2=> {counter2}");
+Console.WriteLine($"3=> {counter3}");
+Console.WriteLine($"4=> {counter4}");
+Console.WriteLine($"5=> {counter5}");
+Console.WriteLine($"6=> {counter6}");
+Console.WriteLine($"7=> {counter7}");
+Console.WriteLine($"8=> {counter8}");
+Console.WriteLine($"9=> {counter9}");
